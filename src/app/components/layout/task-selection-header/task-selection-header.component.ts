15ec7b61d4f362ed4eebca7be24146ce37a1e1c0
nomansd1 <noman.sd114@gmail.com>
2023-09-18 16:15:06 +0500

    update login and created the design of list selection toolbar

--- src/app/components/layout/task-selection-header/task-selection-header.component.ts
+++ src/app/components/layout/task-selection-header/task-selection-header.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./task-selection-header.component.css']
 })
 export class TaskSelectionHeaderComponent {
-
+  toolbarLinks = [
+    { tooltip: 'Set Watchers', url: '', icon: '../../../../assets/watcher.svg' },
+    { tooltip: 'Set Assignees', url: '', icon: '../../../../assets/assignee.svg' },
+    { tooltip: 'Set Status', url: '', icon: '../../../../assets/status.svg' },
+    { tooltip: 'Set Tags', url: '', icon: '../../../../assets/tags.svg' },
+    { tooltip: 'Convert to subtask', url: '', icon: '../../../../assets/subtask.svg' },
+    { tooltip: 'Move tasks or add tasks in multiple Lists', url: '', icon: '../../../../assets/movetask.svg' },
+    { tooltip: 'Duplicate tasks', url: '', icon: '../../../../assets/duplicatetask.svg' },
+    { tooltip: 'Remove tasks from this List', url: '', icon: '../../../../assets/removetask.svg' },
+    { tooltip: 'Set Dates', url: '', icon: '../../../../assets/setdate.svg' },
+    { tooltip: 'Set Priority', url: '', icon: '../../../../assets/toolpriority.svg' },
+    { tooltip: 'Dependencies', url: '', icon: '../../../../assets/dependencies.svg' },
+    { tooltip: 'Merge tasks', url: '', icon: '../../../../assets/mergetask.svg' },
+    { tooltip: 'Tasks Linking', url: '', icon: '../../../../assets/tasklinking.svg' },
+    { tooltip: 'No Custom Fields available', url: '', icon: '../../../../assets/nocustomfield.svg' },
+    { tooltip: 'Archive tasks', url: '', icon: '../../../../assets/arch.svg' },
+    { tooltip: 'Delete', url: '', icon: '../../../../assets/del.svg' },
+  ]
 }
