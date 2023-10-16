584a29b8ea1415ac30186ed7ff7ff1322c0a0a51
nomansd1 <noman.sd114@gmail.com>
2023-10-16 16:53:09 +0500

    working on calendar

--- src/app/components/spaces/calendar-picker/calendar-picker.component.ts
+++ src/app/components/spaces/calendar-picker/calendar-picker.component.ts
@@ -1,10 +1,16 @@
 import { Component, Input } from '@angular/core';
 
+
 @Component({
   selector: 'st-calendar-picker',
   templateUrl: './calendar-picker.component.html',
   styleUrls: ['./calendar-picker.component.css']
 })
 export class CalendarPickerComponent {
-  @Input() isOpen: boolean = false;
+  
+  viewDate: Date = new Date();
+  
+  constructor() {
+    console.log(this.viewDate.getDay());
+  }
 }
