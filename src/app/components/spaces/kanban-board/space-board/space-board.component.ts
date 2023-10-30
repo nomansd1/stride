10c72a9cc4899bdacb34937119bb1a017715cabe
noman <noman.sd114@gmail.com>
2023-10-30 10:28:00 -0700

    updated

--- src/app/components/spaces/kanban-board/space-board/space-board.component.ts
+++ src/app/components/spaces/kanban-board/space-board/space-board.component.ts
@@ -1,4 +1,11 @@
+import { Overlay } from '@angular/cdk/overlay';
 import { Component, Input } from '@angular/core';
+import { CalendarPickerService } from 'src/app/services/calendar-picker.service';
+import { CalendarPickerComponent } from '../../calendar-picker/calendar-picker.component';
+import { ComponentPortal } from '@angular/cdk/portal';
+import { format } from 'date-fns';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'st-space-board',
@@ -12,6 +19,18 @@ export class SpaceBoardComponent {
   createSubTaskToolbar = false;
   createNewTaskToolbar = false;
 
+  public selectedDate: any = [];
+  public overlayRef: any = [];
+  private selectedDateSubscription!: Subscription;
+
+
+
+  constructor(
+    private overlay: Overlay,
+    private calPickService: CalendarPickerService
+  ) {}
+
+
   openCreateSubTaskToolbar() {
     this.createSubTaskToolbar = true;
   }
@@ -24,5 +43,56 @@ export class SpaceBoardComponent {
   }
   closeNewTaskToolbar() {
     this.createNewTaskToolbar = false;
+  }
+
+  // testing for creation of overlay
+  toggleCalendarPicker(index: any) {
+    // Unsubscribe from the previous subscription if it exists
+    if (this.selectedDateSubscription) {
+      this.selectedDateSubscription.unsubscribe();
+    }
+
+    // Subscribe to the selectedDate$ observable
+    this.selectedDateSubscription = this.calPickService.selectedDate$.subscribe(selectedDate => {
+      selectedDate = format(selectedDate, 'MMM dd, HH:m a');
+      this.selectedDate[index] = selectedDate; // Update the selectedDate array at the specified index
+      this.closeCalenderPicker();
+    });
+
+
+    // Toggle the state for the specified row
+    const btnRef: any = document.querySelector(`#overlay${index}`);
+    const overlayConfig = {
+      positionStrategy: this.overlay.position()
+        .flexibleConnectedTo(btnRef)
+        .withPositions([
+          { originX: 'start', originY: 'bottom', overlayX: 'end', overlayY: 'top' },
+          { originX: 'start', originY: 'bottom', overlayX: 'start', overlayY: 'top' },
+        ])
+        .setOrigin(btnRef),
+      backdropClass: 'cdk-overlay-backdrop',
+      hasBackdrop: true
+    }
+
+    this.overlayRef[index] = this.overlay.create(overlayConfig);
+    const dropdownOverlay = new ComponentPortal(CalendarPickerComponent);
+    this.overlayRef[index].attach(dropdownOverlay);
+    console.log(this.overlayRef);
+
+
+    this.overlayRef[index].backdropClick().subscribe(() => {
+      this.overlayRef[index].detach()
+      this.overlayRef[index] = null;
+    });
+    // this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  public closeCalenderPicker() {
+    for (const overlay of this.overlayRef) {
+      if (overlay && overlay.hasAttached()) {
+        overlay.detach(); // Detach the overlay
+        overlay.dispose(); // Dispose of the overlay
+      }
+    }
   }
 }
